3bd907cec05a202046d42bf55270a9f2ddeb8aa5
TotalTaxAmount <shieldscoen@gmail.com>
2024-01-23 13:13:23 -0700

    Add some basic stuff to the climber subsystem

---
+++ src/main/java/frc/robot/constants/ClimberConstants.java
@@ -0,0 +1,6 @@
+package frc.robot.constants;
+
+public class ClimberConstants {
+    public static final int leftSideID = 15;
+    public static final int rightSideID = 16;
+}
--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,6 +1,38 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.ClimberConstants;
 
 public class ClimberSubsystem extends SubsystemBase {
+    private CANSparkMax leftSide = new CANSparkMax(ClimberConstants.leftSideID, CANSparkLowLevel.MotorType.kBrushless);
+    private CANSparkMax rightSide = new CANSparkMax(ClimberConstants.leftSideID, CANSparkLowLevel.MotorType.kBrushless);
+
+    /**
+     * This function sets the state of the climber, possible options are EXTEND, RETRACT, or HOLD
+     * @param state The state we are setting the climber to.
+     */
+    public void setClimberState(ClimberStates state) {
+        switch (state) {
+            case EXTEND -> {
+                // Extend logic
+            }
+            case RETRACT -> {
+                // Retract logic
+            }
+            case HOLD -> {
+                // Hold logic
+            }
+        }
+    }
+
+    /**
+     * Enum of the 3 different climber states
+     */
+    public enum ClimberStates {
+        EXTEND,
+        RETRACT,
+        HOLD
+    }
 }
