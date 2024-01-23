732a7e5452a9dadc9debc6d38ff02016f6c4bc68
TotalTaxAmount <shieldscoen@gmail.com>
2024-01-22 19:32:19 -0700

    Started indexer subsystem

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+public class Constants {
+    public final static int wheel1ID = 11;
+    public final static int wheel2ID = 12;
+    public final static int wheel3ID = 13;
+    public final static int indexerRotateID = 14;
+
+}
---
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -0,0 +1,65 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class IndexerSubsystem extends SubsystemBase {
+    private final CANSparkMax wheel1 = new CANSparkMax(Constants.wheel1ID, CANSparkLowLevel.MotorType.kBrushless);
+
+    private final CANSparkMax wheel2 = new CANSparkMax(Constants.wheel2ID, CANSparkLowLevel.MotorType.kBrushless);
+
+    private final CANSparkMax wheel3 = new CANSparkMax(Constants.wheel3ID, CANSparkLowLevel.MotorType.kBrushless);
+
+    private final CANSparkMax indexerRotate = new CANSparkMax(Constants.indexerRotateID, CANSparkLowLevel.MotorType.kBrushless);
+
+    /**
+     * Set indexer pos changes the position of the indexer for either the Amp or Speaker
+     * @param position Enum for the positions, can be either AMP or SPEAKER
+     */
+    public void setIndexerPos(IndexerPositions position) {
+        switch (position) {
+            case AMP -> {
+                // Logic to set app position
+            }
+            case DOWN -> {
+                // Logic to set speaker position
+            }
+        }
+    }
+
+    /**
+     * rotateMotorVolts rotates one of the motors by setting the voltage applied to the motor
+     * @param motor The target motor, can be WHEEL_1, WHEEL_2, WHEEL_3, or INDEXER_POS
+     * @param volts The target voltage.
+     */
+    public void rotateMotorVolts(IndexerMotors motor, double volts) {
+        switch (motor) {
+            case WHEEL_1 -> wheel1.setVoltage(volts);
+            case WHEEL_2 -> wheel2.setVoltage(volts);
+            case WHEEL_3 -> wheel3.setVoltage(volts);
+            case INDEXER_POS -> indexerRotate.setVoltage(volts);
+        }
+    }
+
+    /**
+     * Enum of the different possible positions for the indexer
+     */
+    public enum IndexerPositions {
+        AMP,
+        DOWN,
+    }
+
+    /**
+     * Enum of possible motors to control
+     */
+    public enum IndexerMotors {
+        WHEEL_1,
+        WHEEL_2,
+        WHEEL_3,
+        INDEXER_POS
+    }
+
+
+}
